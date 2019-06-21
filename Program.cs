2078d8bbd1d8c4d8ca5ec0b2db40405bf2918a5b
Tushar Mohandas <41070654+tushar9427@users.noreply.github.com>
2019-06-21 10:32:13 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace stringReverse1
+{
+    class Program
+    {
+        public static void stringReverseString1()
+        {
+            string word = "", reverseWord = "";
+            Console.WriteLine("Please input the word to be reversed");
+            word = Console.ReadLine();
+            var length = word.Length - 1;
+            while (length >= 0)
+            {
+                reverseWord = reverseWord + word[length];
+                length--;
+            }
+            Console.WriteLine("The output is " + reverseWord);
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            
+            Program.stringReverseString1();
+
+
+        }
+    }
+}
