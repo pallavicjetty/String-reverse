d5660b479ed9e861ef8b8da40dedfe537ef7d19e
Pallavi <45694834+pallavicjetty@users.noreply.github.com>
2019-10-17 12:31:48 +0530

    comment is added

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace stringReverse1
             string word = "", reverseWord = "";
             Console.WriteLine("Please input the word to be reversed");
             word = Console.ReadLine();
+            //variable length holds the length of the word
             var length = word.Length - 1;
             while (length >= 0)
             {
@@ -20,10 +21,7 @@ namespace stringReverse1
         }
         static void Main(string[] args)
         {
-            
             Program.stringReverseString1();
-
-
         }
     }
 }
